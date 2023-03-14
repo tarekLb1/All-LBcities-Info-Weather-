3e5e32f02793f29c2924503224af1278496073a3
tarekbaarini <tarekbaarini02@gmail.com>
2023-03-14 15:04:44 -0700

    Fix time problem (remove toISOString , replace it with padStart())

--- index.js
+++ index.js
@@ -119,11 +119,29 @@
 
                             //Fix the Problem
 
-                            const now = new Date();
-                            console.log(now)
+                    //         const now = new Date();
+                    //         console.log(now)
                     
-                    const currenttimeanddata = now.toISOString().slice(0, 13) + ":00";
+                    // const currenttimeanddata = now.toISOString().slice(0, 13) + ":00";
+                    // console.log(`Current Time & Date: ${currenttimeanddata}`);
+
+                    const date = new Date();
+                    const year = date.getFullYear();
+                    const month = String(date.getMonth() + 1).padStart(2, '0');
+                    const day = String(date.getDate()).padStart(2, '0');
+                    const hours = String(date.getHours()).padStart(2, '0');
+                    const minutes = String(date.getMinutes()).padStart(2, '0');
+                    const seconds = String(date.getSeconds()).padStart(2, '0');
+                    const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+                    const localDateString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
+                    const currenttimeanddata= localDateString.slice(0, 13) + ":00";
                     console.log(`Current Time & Date: ${currenttimeanddata}`);
+
+
+                    
+                    
+
+                    
 
                     const timedata = json.hourly.time;
 
